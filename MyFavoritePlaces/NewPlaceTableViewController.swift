1a6a2621bc2cd5c5f246c92c835f4514b609d5c6
Yaroslav <yaroslav-derugo@yandex.ru>
2021-02-02 17:50:22 +0300

    Adding actionSheet

---
+++ MyFavoritePlaces/NewPlaceTableViewController.swift
@@ -0,0 +1,83 @@
+//
+//  NewPlaceTableViewController.swift
+//  MyFavoritePlaces
+//
+//  Created by Yaroslav on 2.02.21.
+//
+
+import UIKit
+
+class NewPlaceTableViewController: UITableViewController, UINavigationControllerDelegate {
+    
+    
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.tableFooterView = UIView()
+    }
+    
+    // MARK: - Table view delegate
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if indexPath.row == 0 {
+            
+            let actioSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+            
+            let camera = UIAlertAction(title: "Camera", style: .default) { _ in
+                self.chooseImagePicker(source: .camera)
+            }
+            
+            let foto = UIAlertAction(title: "Foto", style: .default) { _ in
+                self.chooseImagePicker(source: .photoLibrary)
+            }
+            
+            let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+            
+            actioSheet.addAction(camera)
+            actioSheet.addAction(foto)
+            actioSheet.addAction(cancel)
+            
+            present(actioSheet, animated: true)
+            
+        } else {
+            view.endEditing(true)
+        }
+    }
+}
+
+// MARK:- Text field delegate
+
+extension NewPlaceTableViewController: UITextFieldDelegate {
+    
+    // Скрываем клавиатуру по нажатию на Done
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+}
+
+// MARK: - Work with image
+
+extension NewPlaceTableViewController: UIImagePickerControllerDelegate {
+    
+    func chooseImagePicker(source: UIImagePickerController.SourceType) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(source) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.allowsEditing = true
+            imagePicker.sourceType = source
+            present(imagePicker, animated: true)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageOfPlace.image = info[.editedImage] as? UIImage
+        imageOfPlace.contentMode = .scaleAspectFit
+        imageOfPlace.clipsToBounds = true
+        dismiss(animated: true)
+    }
+}
