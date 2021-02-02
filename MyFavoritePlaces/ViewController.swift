630c5fc4c44380cfa41b37376709253f7ff03945
Yaroslav <yaroslav-derugo@yandex.ru>
2021-02-02 10:34:17 +0300

    Initial Commit

---
+++ MyFavoritePlaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFavoritePlaces
+//
+//  Created by Yaroslav on 2.02.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
