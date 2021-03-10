da8eab7bfd1bd85a70e71e477896c3d97ba89654
Yaroslav <Yaraslav.IOS.Developer@gmail.com>
2021-03-10 20:22:04 +0300

    Additing  MapViewController

---
+++ MyFavoritePlaces/MapViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MapViewController.swift
+//  MyFavoritePlaces
+//
+//  Created by Yaroslav on 10.03.2021.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+   
+    @IBAction func closeViewController() {
+        dismiss(animated: true)
+    }
+    
+}
